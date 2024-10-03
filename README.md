42f7280b6513d50e67872f3cf3f357c6d031c7d8
iiPython <ben@iipython.dev>
2024-10-02 23:33:07 -0500

    well lets see how this does

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # iiPythonx / USPS
+
+A CLI for tracking packages from USPS.
+
+### Installation
+
+```sh
+uv pip install git+https://github.com/iiPythonx/usps
+```
+
+### Usage
+
+Get the tracking information for a package:
+```sh
+usps track <tracking number>
+```
+
+Add a tracking number to your package list:
+```sh
+usps add <tracking number>
+```
+
+Remove a tracking number from your package list:
+```sh
+usps remove <tracking number>
+```
+
+Show all your current packages:
+```sh
+usps track
+```
+
+### Inspiration
+
+I tried to make a basic web scraper for the USPS website months ago, only to find out that its security is crazy.  
+Instead of trying to reverse engineer their client, this script generates cookies using Selenium and then uses requests
+and BeautifulSoup to make the request and parse the result.
--- usps/__main__.py
+++ usps/__main__.py
@@ -9,17 +9,13 @@ from rich.console import Console
 from usps.storage import packages
 from usps.tracking import Package
 
-from .commands import usps, DefaultCommandGroup
+from .commands import usps
 from .commands.utils import get_delta
 
 # Initialization
 con = Console(highlight = False)
 
 # Handle commands
-@usps.group(cls = DefaultCommandGroup, name = "track", default_group = True)
-def group_track() -> None:
-    return
-
 def show_package(tracking_number: str, package: Package) -> None:
     con.print(f"°︎ USPS [bright_blue]{tracking_number}[/] - [cyan]{package.state}[/]")
     if package.expected:
@@ -38,9 +34,12 @@ def show_package(tracking_number: str, package: Package) -> None:
 
     print()
 
-@group_track.command("show", default_command = True)
+@usps.command("track", default_command = True)
 @click.argument("tracking-number", required = False)
-def command_show(tracking_number: str | None) -> None:
+def command_track(tracking_number: str | None) -> None:
+    """Track the specified tracking numbers, tracking your package list if no tracking
+    number is specified."""
+
     from .tracking import tracking
 
     if tracking_number is not None:
@@ -53,9 +52,22 @@ def command_show(tracking_number: str | None) -> None:
     for package in tracking_numbers:
         show_package(package, tracking.track_package(package))
 
-@group_track.command("add")
+@usps.command("add")
 @click.argument("tracking-numbers", nargs = -1)
 def command_add(tracking_numbers: tuple[str]) -> None:
+    """Add tracking numbers to your package list."""
     packages.save(packages.load() + list(tracking_numbers))
     for tracking_number in tracking_numbers:
         con.print(f"[green]✓ USPS {tracking_number} added to your package list.[/]")
+
+@usps.command("remove")
+@click.argument("tracking-numbers", nargs = -1)
+def command_remove(tracking_numbers: tuple[str]) -> None:
+    """Remove tracking numbers from your package list."""
+    current_packages = packages.load()
+    for tracking_number in tracking_numbers:
+        if tracking_number in current_packages:
+            current_packages.remove(tracking_number)
+            con.print(f"[green]✓ USPS {tracking_number} removed from your package list.[/]")
+
+    packages.save(current_packages)
