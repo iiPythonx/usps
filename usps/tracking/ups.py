ea01d29286e141ea40f56174b030d35a03224e0b
iiPython <ben@iipython.dev>
2024-10-10 19:40:44 -0500

    add expected delivery support to ups and add some status codes

--- usps/tracking/ups.py
+++ usps/tracking/ups.py
@@ -9,9 +9,34 @@ from usps.utils import LOCAL_TIMEZONE
 from . import USER_AGENT, Package, Step
 from .exceptions import StatusNotAvailable
 
+# Handle mapping
+UPS_CMS_MAPPINGS = {
+    "cms.stapp.jan": "January",
+    "cms.stapp.feb": "February",
+    "cms.stapp.mar": "March",
+    "cms.stapp.apr": "April",
+    "cms.stapp.may": "May",
+    "cms.stapp.jun": "June",
+    "cms.stapp.jul": "July",
+    "cms.stapp.aug": "August",
+    "cms.stapp.sep": "September",
+    "cms.stapp.oct": "October",
+    "cms.stapp.nov": "November",
+    "cms.stapp.dec": "December"
+}
+
 # Main class
 class UPSTracking:
     _session: Session | None = None
+
+    @staticmethod
+    def __map_milestone_name(milestone: str) -> str:
+        match milestone.lower():
+            case "we have your package":
+                return "Has Package"
+
+            case _:
+                return milestone
 
     @classmethod
     def track_package(cls, tracking_number: str) -> Package:
@@ -36,15 +61,32 @@ class UPSTracking:
 
         data = response["trackDetails"][0]
 
+        # Handle estimated delivery date
+        estimated_delivery = None
+        if data["packageStatusTime"]:
+            delivery = data["scheduledDeliveryDateDetail"]
+            month, year = UPS_CMS_MAPPINGS[delivery["monthCMSKey"]], datetime.now().year
+            estimated_delivery = [
+                datetime.strptime(f"{month} {delivery['dayNum']} {time.replace('.', '')}", "%B %d %I:%M %p").replace(year = year)
+                for time in data["packageStatusTime"].split(" - ")
+            ]
+
+        # Make up some status names
+        latest_scan = data["shipmentProgressActivities"][0]
+        status_name = latest_scan["activityScan"]
+        match data["packageStatusCode"]:
+            case "160":
+                status_name = f"Your package has arrived in {latest_scan['location']} and is getting ready for shipping."
+
         # Bundle together
         return Package(
-            None,
-            data["shipmentProgressActivities"][0]["activityScan"],
+            estimated_delivery,
+            status_name,
             [x for x in data["milestones"] if x["isCurrent"]][-1]["name"],
             [
                 Step(
-                    step["milestoneName"]["name"],
-                    step["location"],
+                    cls.__map_milestone_name(step["milestoneName"]["name"]),
+                    step["location"].replace("United States", "US") if "," in step["location"] else "",
                     datetime.strptime(f"{step['gmtDate']} {step['gmtTime']}", "%Y%m%d %H:%M:%S").replace(tzinfo = LOCAL_TIMEZONE) +\
                          timedelta(hours = int(step["gmtOffset"].split(":")[0]))
                 )
